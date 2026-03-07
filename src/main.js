0bfa99a0c947a476b389ec7eab87d882e1352dc8
Vladyslav.Tverdokhliebov@hermes-europe.co.uk <Vladyslav.Tverdokhliebov@evri.com>
2026-03-07 02:14:02 +0200

    release 1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Phaser from 'phaser';
+import Phaser from '/phaser';
 import StartScene from './game/scenes/StartScene.js';
 import MainScene from './game/scenes/MainScene.js';
 import VictoryScene from './game/scenes/VictoryScene.js';
