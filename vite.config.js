4fac5cfb797f8303c0f3aece40a3a6973d9f9a19
Vladyslav.Tverdokhliebov@hermes-europe.co.uk <Vladyslav.Tverdokhliebov@evri.com>
2026-03-09 10:36:30 +0200

    release 1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/tanks/',
-    plugins: [react()],
     build: {
         outDir: 'dist',
         assetsDir: 'assets',
